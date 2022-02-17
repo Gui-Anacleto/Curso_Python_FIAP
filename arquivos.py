21b801be56b0b44ccb46b938f20b56a4b4ec9d71
Guilherme de Freitas Anacleto <48931728+Paquista0@users.noreply.github.com>
2022-02-17 20:43:46 -0300

    Delete arquivos.py

--- arquivos.py
+++
@@ -1,8 +0,0 @@
-bd = []
-with open("iris.data", "r") as arquivo:
-    for registro in arquivo.readlines():
-        bd.append(registro.split(","))
-
-print(bd)
-
-print(float(bd[0] [0]) + float(bd[0] [1]))
