a64b9782eccb08baab8f9a83645f7f5c4536a68f
Guilherme de Freitas Anacleto <48931728+Paquista0@users.noreply.github.com>
2022-02-17 20:45:04 -0300

    Add files via upload

---
+++ Manipulando_Arquivos/encontratexto.py
@@ -0,0 +1,6 @@
+texto = "Eu amo Python"
+
+print(texto[texto.find("o")+1:].find("o"))
+
+print(texto.split(" "))
+
---
+++ Manipulando_Arquivos/texto.py
@@ -0,0 +1,9 @@
+txt= "Anakin e o Vaider"
+
+print(txt[0:6])
+print(txt[9:])
+print(txt[-6:])
+print(txt[::-1])
+
+#0:4:2
+#posicao inicial : quantidade de informacao a percorrer : passo
